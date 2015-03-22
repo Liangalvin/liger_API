4221c65eb701fbe49c89cb942d525fe372a516d2
Alvin Liang <liangalvin@yahoo.com>
2015-03-22 15:41:30 -0400

    Basic bar chart

--- mods.js
+++ mods.js
@@ -1,9 +1,9 @@
 var routes = {
   devSal: function(req, res){
     salaries = {
-      min: "$34,103",
-      median: "$58,583",
-      max: "$88,736",
+      min: 34103,
+      median: 58583,
+      max: 88736,
       descrip: "A web designer/developer is responsible for the design, layout and coding of a website. They are involved with the technical and graphical aspects of a website - how the site works and how it looks. They can also be involved with the maintenance and update of an existing site."
     }
 
@@ -12,9 +12,9 @@ var routes = {
 
   pmSal: function(req, res){
     salaries = {
-      min: "$45,676",
-      median: "78,169",
-      max: "111,392",
+      min: 45676,
+      median: 78169,
+      max: 111392,
       descrip: "The Product Manager is responsible for the product planning and execution throughout the product lifecycle, including: gathering and prioritizing product and customer requirements, defining the product vision, and working closely with engineering, sales, marketing and support to ensure revenue and customer satisfaction goals are met. The Product Manager’s job also includes ensuring that the product supports the company’s overall strategy and goals."
     }
 
@@ -23,9 +23,9 @@ var routes = {
 
   desSal: function(req, res){
     salaries = {
-      min: "47,119",
-      median: "61,283",
-      max: "73,238",
+      min: 47119,
+      median: 61283,
+      max: 73238,
       descrip: "UX designers are primarily concerned with how the product feels. A given design problem has no single right answer. UX designers explore many different approaches to solving a specific user problem. The broad responsibility of a UX designer is to ensure that the product logically flows from one step to the next. One way that a UX designer might do this is by conducting in-person user tests to observe one's behavior. By identifying verbal and nonverbal stumbling blocks, they refine and iterate to create the \"best\" user experience."
     }
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
-
 var routes = require('./mods.js')
+
+app.use(cors());
 
 app.get('/dev', routes.devSal);
 
